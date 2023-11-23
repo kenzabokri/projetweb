1e4f31e474e47b4384ebd313603b6a6ca2eccb1e
hejerayadi <131679852+hejerayadi@users.noreply.github.com>
2023-11-23 22:38:40 +0100

    Add files via upload

---
+++ modele/categorie.php
@@ -0,0 +1,43 @@
+<?php
+class Categorie
+{
+    private ?int $id_cat = null;
+    private ?string $nom_cat = null;
+    private ?string $url_cat = null;
+
+    public function __construct($id = null, $nom, $url)
+    {
+        $this->id_cat = $id;
+        $this->nom_cat = $nom;
+        $this->url_cat = $url;
+    }
+
+    public function getIdCat()
+    {
+        return $this->id_cat;
+    }
+
+    public function getNomCat()
+    {
+        return $this->nom_cat;
+    }
+
+    public function setNomCat($nom_cat)
+    {
+        $this->nom_cat = $nom_cat;
+
+        return $this;
+    }
+
+    public function getUrlCat()
+    {
+        return $this->url_cat;
+    }
+
+    public function setUrlCat($url_cat)
+    {
+        $this->url_cat = $url_cat;
+
+        return $this;
+    }
+}
---
+++ modele/cours2.php
@@ -0,0 +1,55 @@
+<?php
+class Courses
+{
+    private ?int $id_cours = null;
+    private ?int $categorie = null;
+    private ?string $nom_cours = null;
+    private ?float $prix_cours = null;
+
+    public function __construct($id = null, $categorie,$nom, $prix)
+    {
+        $this->id_cours = $id;
+        $this->categorie = $categorie;
+        $this->nom_cours = $nom;
+        $this->prix_cours = $prix;
+    }
+
+    public function getIdCours()
+    {
+        return $this->id_cours;
+    }
+
+    public function getCategorie()
+    {
+        return $this->categorie;
+    }
+
+    public function setCategorie($categorie)
+    {
+        $this->categorie = $categorie;
+
+        return $this;
+    }
+    public function getNomCours()
+    {
+        return $this->nom_cours;
+    }
+
+    public function setNomCours($nom_cours)
+    {
+        $this->nom_cours = $nom_cours;
+
+        return $this;
+    }
+    public function getPrixCours()
+    {
+        return $this->prix_cours;
+    }
+
+    public function setPrixCours($prix_cours)
+    {
+        $this->prix_cours = $prix_cours;
+
+        return $this;
+    }
+}

df855992566f9a271137dfeebb3be91b6f94d4c1
bechir2003 <125089680+bechir2003@users.noreply.github.com>
2023-12-08 00:26:42 +0100

    Delete view directory

--- view/back office/deleteOeuvre.php
+++
@@ -1,6 +0,0 @@
-<?php
-include 'C:/xampp/htdocs/gestio_oeuvre2/controller/oeuvreC.php';
-$cou = new oeuvreC();
-$cou->deleteoeuvre($_GET["id_oeuvre"]);
-header('Location:gestionOeuvre.php');
-?>
--- view/back office/deletecategory.php
+++
@@ -1,6 +0,0 @@
-<?php
-include 'C:/xampp/htdocs/gestio_oeuvre2/controller/categorieC.php';
-$cou = new CategorieC();
-$cou->deleteCategory($_GET["ID_cat"]);
-header('Location:showcategory.php');
-?>
